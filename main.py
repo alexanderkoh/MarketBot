8370ec54b055a6225c7335e493cae57809ee0cc3
Alex <68964818+alexanderkoh@users.noreply.github.com>
2022-02-21 08:03:48 +0000

    Bot resolves BTC live price with command

--- main.py
+++ main.py
@@ -2,6 +2,16 @@
 
 import discord
 import os
+import requests
+import json
+
+def get_price():
+  response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=USD')
+  #response = requests.get('https://zenquotes.io/api/random')
+  json_data = json.loads(response.text)
+  price = str(json_data['bitcoin']['usd'])
+  price_response = "Bitcoin's Price is: " + price + " USD"
+  return(price_response)
 
 my_secret = os.environ['TOKEN']
 
@@ -16,7 +26,9 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+  if message.content.startswith('$btc'):
+    price = get_price()
+    await message.channel.send(price)
+  
 
 client.run(my_secret)
